8a3a32a003de4691a67796b23fa9d384c05afe04
superpenshine <superpenshine@gmail.com>
2019-06-24 17:59:09 -0700

    save

--- network_pt.py
+++ network_pt.py
@@ -79,7 +79,7 @@ class network(object):
         '''
         Load the tensorboard writter
         '''
-        self.writer = SummaryWriter(log_dir=str(self.log_dir), flush_secs=120)
+        self.writer = SummaryWriter(logdir=str(self.log_dir), flush_secs=120)
 
 
     def load_data(self):
