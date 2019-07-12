578e72a38d2ba88437ead39b72ccbf1069605f3a
superpenshine <superpenshine@gmail.com>
2019-07-11 19:25:06 -0700

    save

--- model.py
+++ model.py
@@ -98,6 +98,7 @@ class ResNet(nn.Module):
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
+        x = self.layer4(x)
         x = self.out_conv(x)
 
         return x
